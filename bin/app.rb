c5a0c7af56677e8b1b33ddc623d322c8bbdf3e9c
Kate Klemp <kklemp@misdepartment.com>
2015-03-04 15:47:15 -0700

    Form validation test methods - failures

--- bin/app.rb
+++ bin/app.rb
@@ -1,29 +1,45 @@
 require 'sinatra'
 require 'pony'
+require 'data_mapper'
 
 set :port, 8080
 set :static, true
 set :public_folder, "static"
 set :views, "views"
 
+class Email
+
+  include DataMapper::Resource
+
+  property :email, String, :format => :email_address, :required => true
+
+end
+
+enable :sessions
+use Rack::Session::Cookie, :expire_after => 2592000,
+                           :secret => 'fc683cd9ed1990ca2ea10b84e5e6fba048c24929'
+
 get '/' do
   erb :email_form
 end
 
 post '/' do
-  validates_format_of params[:email], :with => /@/
+
+  session[:name] = params[:name]
+  session[:email] = params[:email]
+  session[:body] = params[:body]
 
   Pony.mail({
     :to => "info@misdepartment.com",
-    :from => params[:email],
+    :from => session[:email],
     :subject => "Inquiry from MIS Department website",
-    :body => "From: " + params[:name] + "
-    Message: " + params[:body],
+    :body => "From: " + session[:name] + "
+    Message: " + session[:body],
     :via => :smtp
     })
   redirect '/success'
 end
 
 get '/success' do
-  erb :index
+  erb :index, :locals => {:name => session[:name], :email => session[:email], :body => session[:body]}
 end
