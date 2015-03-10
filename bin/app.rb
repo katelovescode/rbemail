20203156076ee8e2347f055e6aced5379b61c7c2
Kate Klemp <kklemp@misdepartment.com>
2015-03-10 17:08:31 -0500

    Finally got variable to pull from form fields, thanks Drupal.

--- bin/app.rb
+++ bin/app.rb
@@ -1,60 +1,69 @@
 require 'sinatra'
 require 'pony'
 
+# ruby configs
 set :port, 8080
 set :static, true
 set :public_folder, "static"
 set :views, "views"
 
-get '/' do
-  erb :email_form
-end
+# while true; do curl --data "submitted[your_name]=kate&submitted[your_e_mail_address]=kklemp@misdepartment.com&submitted[subject]=what" http://localhost:8080; sleep 5; done
 
 post '/' do
 
-  # create a variable for each form field
-  # name = "submitted[your_name]".to_sym
-  # email = "submitted[your_e_mail_address]".to_sym
-  # subject = "submitted[subject]".to_sym
-  # reason = "submitted[reason_for_contact]".to_sym
-  # message = "submitted[message]".to_sym
+  # loops through all "submitted" form fields based on Drupal weird form names as submitted["actual name"]
+  form = params[:submitted]
 
-  reqparams = [:name, :email, :subject, :reason, :message]
+  # empty responses array
   responses = []
 
-  reqparams.each do |r|
-    unless r.nil?
-      responses.push([r,true])
+  # array of ["actual name"] values of required fields (still based on Drupal that gives forms weird names)
+  reqpar = [
+    "your_name",
+    "your_e_mail_address",
+    "subject",
+    "reason_for_contact",
+    "message"
+  ]
+
+  # loops through all required parameters
+  reqpar.each do |k|
+    # checks for nil on each parameter value - if not nil, add "key name" with "true" value for responses array, puts values for testing
+    unless form[k].nil?
+      responses.push([k,true])
+      puts k + " " + form[k]
+    # if nil, add "key name" with "false" value for responses array
+    else
+      responses.push([k,false])
+      puts k + " empty"
     end
   end
 
-  puts responses
+ # separate logic for required & non-required fields
+ # kill it if required are missing
+ # regex the email address
+ # validate other fields
+ # if all fields are OK, Pony.mail, if not, don't mail but send JSON
+ # return file data as JSON - show success or specific failed fields
 
-  to = "info@misdepartment.com"
-  name = params[:name]
-  from = params[:email]
-  subject = params[:subject]
-  reason = params[:reason]
-  mailbody = [
-    :reason,
-    :message
-  ]
 
-  # loop through all body variables and concatenate to string with \n in between
-  # unless
-  #   mailbody.each do |e|
-  #     body << params[e]
-  #   end
-  # end
+    # use the below as an example to concatenate string
 
-  # validate name, email and body
+    # loop through all body variables and concatenate to string with \n in between
+    # unless
+    #   mailbody.each do |e|
+    #     body << params[e]
+    #   end
+    # end
 
-  # if validation passes
 
-  Pony.mail({
-    to: to,
-    from: from,
-    subject: subject,
-    body: body
-  })
+    # use the below to send mail
+
+    # Pony.mail({
+    #   to: to,
+    #   from: from,
+    #   subject: subject,
+    #   body: body
+    # })
+
 end
