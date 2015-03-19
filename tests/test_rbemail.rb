7fc00b8bc2fe439061da910b441a2041895d362a
Kate Klemp <kklemp@misdepartment.com>
2015-03-19 17:55:20 -0500

    Code cleanup, minimize, fieldpass logic to send, JSON outputs

--- tests/test_rbemail.rb
+++ tests/test_rbemail.rb
@@ -26,3 +26,37 @@ class EmailTest < Test::Unit::TestCase
     assert last_response.body.include?("I just wanted to say")
   end
 end
+
+
+########################################
+# TESTING CONSOLE COMMANDS
+########################################
+
+# this will die (9393 port - shotgun)
+# while true; do curl --data "submitted[your_name]=kate&submitted[your_e_mail_address]=kklemp@misdepartment.com&submitted[subject]=what" http://localhost:9393; sleep 10; done
+
+# this will pass (9393 port - shotgun)
+# while true; do curl --data "submitted[your_name]=kate&submitted[your_e_mail_address]=kklemp@misdepartment.com&submitted[subject]=what&submitted[reason_for_contact]=becuz" http://localhost:9393; sleep 10; done
+
+# form params with no drupal submitted array (9393 port - shotgun)
+# while true; do curl --data "name=kate&email=kklemp@misdepartment.com&subject=what&reason=becuz" http://localhost:9393; sleep 10; done
+
+########################################
+# TESTING ERB TEMPLATES W/ FORMS - USING BROWSER
+########################################
+
+# get '/' do
+#   # this is for testing non-drupal form names
+#   erb :email_form
+#
+#   # erb :drupal_form
+# end
+
+# for testing the JSON output in the terminal, uncomment to see prettified JSON
+# $parsed = JSON.parse($j)
+# puts JSON.pretty_generate($parsed)
+
+# test JSON output into a file - uncomment below
+# File.open("data/temp.json","w") do |f|
+#   f.write($j)
+# end
