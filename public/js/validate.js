2ba225fe03beed6bf908f765a7797e11387a531f
Kate Klemp <kklemp@misdepartment.com>
2015-03-24 07:32:27 -0500

    Round one of JS handling of JSON from app

--- public/js/validate.js
+++ public/js/validate.js
@@ -2,17 +2,32 @@
 // <input id="edit-submit" name="op" value="Send Message" class="form-submit" type="submit">
 
 $('#webform-client-form-21').submit(function(event) {
-    event.preventDefault();
-    var myreq = $.ajax({
-        type:"POST",
-        url:"http://localhost:9393/",
-        dataType:"text",
-        data:$('#webform-client-form-21').serialize()
+  event.preventDefault();
+  var request = $.ajax({
+    url: "http://localhost:9393/",
+    type: "POST",
+    dataType: "json",
+    data: $('#webform-client-form-21').serialize()
+  });
+
+  request.done(function(msg) {
+    var myClass = "thisIsMyClass";
+    var div = $("<div>Here</div>").addClass(myClass);
+
+
+    msg.map(function (field) {
+      if (field.formpass == false) {
+        $("input[name=submitted\\[" + field.fieldname + "\\]]").addClass("error");
+      } else {
+        $("input[name=submitted\\[" + field.fieldname + "\\]]").removeClass("error");
+      }
     });
-    myreq.done(function() {
-        alert("done");
-    });
-    myreq.always(function() {
-        alert("always");
-    });
+
+
+  });
+
+  request.fail(function(jqXHR, textStatus) {
+    alert( "Request failed: " + textStatus );
+  });
+  return false;
 });
