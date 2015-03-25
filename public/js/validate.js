2921d6c7ce1893e8c3b3b26a26e1c6f39493c118
Kate Klemp <kklemp@misdepartment.com>
2015-03-25 10:59:25 -0500

    Conditional error messages

--- public/js/validate.js
+++ public/js/validate.js
@@ -1,39 +1,64 @@
 // Drupal form submit for reference
 // <input id="edit-submit" name="op" value="Send Message" class="form-submit" type="submit">
 
-$('#webform-client-form-21').submit(function(event) {
-  event.preventDefault();
-  var request = $.ajax({
-    url: "http://localhost:9393/",
-    type: "POST",
-    dataType: "json",
-    data: $('#webform-client-form-21').serialize()
-  });
+$( function() {
 
-  request.done(function(msg) {
+  $('#webform-client-form-21').submit(function(event) {
 
-    var arraynames = []
+    // prevent click from refreshing page
+    event.preventDefault();
 
-    msg.map(function (field) {
-      if (field.formpass == false) {
+    // ajax call
+    $.ajax({
+      url: "http://localhost:9393/",
+      type: "POST",
+      dataType: "json",
+      data: $('#webform-client-form-21').serialize(),
+      success: function(data) {
 
-        arraynames.push([field.fieldname,field.formpass]);
-//        $("input[name=submitted\\[" + field.fieldname + "\\]]").addClass("error");
-      } else {
-//        $("input[name=submitted\\[" + field.fieldname + "\\]]").removeClass("error");
+        // create an error message
+        function createError(location,errormsg) {
+          $(location).addClass("error");
+          if ($(location).next().is("div.errorDiv")) {
+            $(".errorDiv").text(errormsg);
+          } else {
+            $(location).after("<div class='errorDiv'>" + errormsg + "</div>");
+          }
+        }
+
+        // remove error message if form input is valid
+        function removeError(location) {
+          $(location).removeClass("error");
+          if ($(location).next().is("div.errorDiv")) {
+            $(".errorDiv").remove();
+          }
+        }
+
+
+        $.each(data, function(i,field) {
+
+          var nameSearch = "[name*=" + field.fieldname + "]";
+
+          switch(field.formpass) {
+            case 0:
+              console.log(field.fieldname + ": good to go");
+              removeError(nameSearch);
+              break;
+            case 1:
+              console.log(field.fieldname + ": error code 1");
+              removeError(nameSearch);
+              createError(nameSearch,"error code " + field.formpass);
+              break;
+            case 2:
+              console.log(field.fieldname + ": error code 2");
+              removeError(nameSearch);
+              createError(nameSearch,"error code " + field.formpass);
+              break;
+            default:
+              console.log("Something went wrong, field " + field.fieldname + "is missing");
+          }
+        })
       }
     });
-
-    arraynames.map(function (fld) {
-      fldnm = fld[0];
-      fldcl = fld[1];
-      $("input[name=submitted\\[" + fldnm + "\\]]").addClass("" + fldcl);
-    })
-
   });
-
-  request.fail(function(jqXHR, textStatus) {
-    alert( "Request failed: " + textStatus );
-  });
-  return false;
 });
