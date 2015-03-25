36b6a87d82e2cea30c9f96fab4b2238d8a59ebe7
Kate Klemp <kklemp@misdepartment.com>
2015-03-25 11:10:44 -0500

    Comment to explain inconsistencies

--- public/js/validate.js
+++ public/js/validate.js
@@ -16,6 +16,18 @@ $( function() {
       data: $('#webform-client-form-21').serialize(),
       success: function(data) {
 
+
+        /* Somewhere in the conditionals of these functions, there are inconsistencies.
+        Submit a blank form; existing data should report an error code 1 for name, email and subject.
+        This creates the .errorDiv with text "error code 1", and applies the .error class to the input field, coloring it red
+        If you fill in name and submit again, it should remove the .errorDiv and .error class
+        If you empty the name and submit again, it will apply the .error class, but it will not re-create the .errorDiv
+        This appears to happen in all fields; if they are submitted correctly once, they cannot create the .errorDiv if they are emptied
+
+        Also, if you fill in a correct email, then switch to an incorrect email, the error code text should change from 1 to 2 (this is desired behavior)
+        Please verify that's still happening after troubleshooting
+        */
+
         // create an error message
         function createError(location,errormsg) {
           $(location).addClass("error");
@@ -34,7 +46,7 @@ $( function() {
           }
         }
 
-
+        // loop through submitted data to apply or remove error class and div
         $.each(data, function(i,field) {
 
           var nameSearch = "[name*=" + field.fieldname + "]";
