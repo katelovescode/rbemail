05d3e05ce7b8f3703abe215ca1cfc3086bfccd51
Kate Klemp <kklemp@misdepartment.com>
2015-05-12 12:00:18 -0500

    Use fieldarray context

--- spec/rbemail_spec.rb
+++ spec/rbemail_spec.rb
@@ -13,47 +13,50 @@ require File.expand_path('../spec_helper', __FILE__)
 
 describe "Rbemail" do
   include Rack::Test::Methods
-  let(:example_email) { "email@example.com" }
-  let(:good_request) {
-    {
-      example_fieldarray: {
-        example_name: "test",
-        example_email: example_email,
-        example_message: "hello world",
-        example_phone: "123-456-7890",
-        example_rating: "4"
+
+  context "field array is present, such as in drupal" do
+    let(:example_email) { "email@example.com" }
+    let(:good_request) {
+      {
+        example_fieldarray: {
+          example_name: "test",
+          example_email: example_email,
+          example_message: "hello world",
+          example_phone: "123-456-7890",
+          example_rating: "4"
+        }
       }
     }
-  }
-  let(:mailcatcher_endpoint) { "http://127.0.0.1:1080" }
+    let(:mailcatcher_endpoint) { "http://127.0.0.1:1080" }
 
-  # set up mailcatcher for the tests
-  before do
-    `mailcatcher`
+    # set up mailcatcher for the tests
+    before do
+      `mailcatcher`
+    end
+
+    # tear it down!
+    after do
+      `kill $(ps -ef | grep mailcatcher | grep ruby | awk '{ print $2 }')`
+    end
+
+    def app
+      Sinatra::Application
+    end
+
+    it "accepts POST requests at root" do
+      post "/", good_request
+      expect(last_response.status).to equal(200)
+    end
+
+    it "produces a real email" do
+      post "/", good_request
+      uri = URI.parse("#{mailcatcher_endpoint}/messages")
+      response = Net::HTTP.get_response(uri)
+      messages = JSON.parse(response.body)
+      last_message = messages[0]
+      expect(last_message['sender']).to eq("<#{example_email}>")
+    end
+
   end
-
-  # tear it down!
-  after do
-    `kill $(ps -ef | grep mailcatcher | grep ruby | awk '{ print $2 }')`
-  end
-
-  def app
-    Sinatra::Application
-  end
-
-  it "accepts POST requests at root" do
-    post "/", good_request
-    expect(last_response.status).to equal(200)
-  end
-
-  it "produces a real email" do
-    post "/", good_request
-    uri = URI.parse("#{mailcatcher_endpoint}/messages")
-    response = Net::HTTP.get_response(uri)
-    messages = JSON.parse(response.body)
-    last_message = messages[0]
-    expect(last_message['sender']).to eq("<#{example_email}>")
-  end
-
 
 end
